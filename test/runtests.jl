4b88f56ab524b80b4d11ab5def7b1e8b93975a74
Ian Butterworth <i.r.butterworth@gmail.com>
2025-01-25 11:10:27 -0500

    wrap distributed_exec subprocess tests in a testset, with cmd as context

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,14 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
+using Test
+
 # Run the distributed test outside of the main driver since it needs its own
 # set of dedicated workers.
 include(joinpath(Sys.BINDIR, "..", "share", "julia", "test", "testenv.jl"))
 disttestfile = joinpath(@__DIR__, "distributed_exec.jl")
 
-cmd = `$test_exename $test_exeflags $disttestfile`
-
-if !success(pipeline(cmd; stdout=stdout, stderr=stderr)) && ccall(:jl_running_on_valgrind,Cint,()) == 0
-    error("Distributed test failed, cmd : $cmd")
+@testset let cmd = `$test_exename $test_exeflags $disttestfile`
+    @test success(pipeline(cmd; stdout=stdout, stderr=stderr)) && ccall(:jl_running_on_valgrind,Cint,()) == 0
 end
 
 include("managers.jl")
